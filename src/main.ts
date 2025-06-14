ff55088b5db2f433e9a882a7d7185852c70c2f34
goldenhub <goldenazubuike@gmail.com>
2025-06-14 21:56:13 +0100

    fix: add global prefix api/v1

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  app.setGlobalPrefix('/api/v1');
   await app.listen(process.env.PORT ?? 4321);
 }
 void bootstrap();
