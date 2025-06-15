4e3e302b2876e35cfc1b61d4da6999b8eda44dce
goldenhub <goldenazubuike@gmail.com>
2025-06-15 22:10:33 +0100

    fix: changed failed login response

--- src/auth/local-auth.strategy.ts
+++ src/auth/local-auth.strategy.ts
@@ -1,7 +1,7 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
-import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
