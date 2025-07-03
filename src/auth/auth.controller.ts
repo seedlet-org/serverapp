0fad90d5dd8c0cb1fc00d1babdfd62bb8e83a9d3
goldenhub <goldenazubuike@gmail.com>
2025-07-03 01:14:19 +0100

    update: logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -167,7 +167,6 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      path: '/auth/refresh',
     });
 
     return {
