c0c622451cbef1edaea65bcabd20a8fad4d883a5
goldenhub <goldenazubuike@gmail.com>
2025-07-26 01:50:47 +0100

    update: updated role guard

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -4,12 +4,13 @@ import { ROLES_KEY } from '../decorators/roles.decorator';
 import { Request } from 'express';
 import prisma from 'src/prisma/prisma.middleware';
 import { User } from '@prisma/client';
+import { userWithRole } from '../types';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const requiredRoles: string[] = this.reflector.get<string[]>(
       ROLES_KEY,
       context.getHandler(),
@@ -19,18 +20,19 @@ export class RolesGuard implements CanActivate {
 
     const request = context.switchToHttp().getRequest<Request>();
 
-    const user = request.user as User;
+    const user = request.user as userWithRole;
+    const role = user.role;
 
-    if (!user || !user.roleId) {
+    if (!user || !role || !role.id) {
       return false;
     }
 
-    const role = await prisma.role.findUnique({
-      where: { id: user.roleId },
-      select: { name: true },
-    });
+    // const role = await prisma.role.findUnique({
+    //   where: { id: user.roleId },
+    //   select: { name: true },
+    // });
 
-    if (!role) return false;
+    // if (!role) return false;
 
     return requiredRoles.includes(role.name);
   }
